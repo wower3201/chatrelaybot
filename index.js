91edea09c9ffe21adaeb981361d141a02c1bddec
wower3201 <84767488+wower3201@users.noreply.github.com>
2022-09-12 17:31:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ client.on('text', data => {
 })
 
 bot.on("messageCreate", message => {
+  if (message.author.id == "1018467183933800518")
 if (message.channelId == "1018496267309031504") {
   client.queue('text', {
     type: 'chat', needs_translation: false, source_name: client.username, xuid: '', platform_chat_id: '',
