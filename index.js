a37a47885ce0ae46d9a0369aba4af0241cf44ee5
wower3201 <84767488+wower3201@users.noreply.github.com>
2022-09-16 14:39:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,33 @@ bot.login(process.env.token)
 const bedrock = require('bedrock-protocol')
 const client = bedrock.createClient({
   realms: {
-    realmInvite: "drqP2b_b2b8"// Function which recieves an array of joined/owned Realms and must return a single Realm. Can be async
+    realmInvite: "drqP2b_b2b8",
+    }
   }
-})
+)
 
 
 client.on('text', data => {
   bot.channels.cache.get('1018496267309031504').send(`[${data.source_name}]: ${data.message}`)
-})
+  if (data.message === "§e%multiplayer.player.left.realms" && data.source_name === "undefined") {
+    const ilcoso = new Discord.MessageEmbed()
+    .setTitle("Info sistema") //Titolo
+    .setColor("#34a42d") // Colore principale
+    .setDescription("Un player ha abbandonato il realm")
+
+    bot.channels.cache.get('1018496267309031504').send({embeds: [ilcoso]})
+  } 
+  
+  if (data.message === "§e%multiplayer.player.joined.realms" && data.source_name === "undefined") {
+      const ilcoso2 = new Discord.MessageEmbed()
+      .setTitle("Info sistema") //Titolo
+      .setColor("#34a42d") // Colore principale
+      .setDescription("Un player si è unito nel realm")
+  
+      bot.channels.cache.get('1018496267309031504').send({embeds: [ilcoso2]})
+    }
+  }
+  )
 
 bot.on("messageCreate", message => {
   if (message.author.id == "1018467183933800518") return
